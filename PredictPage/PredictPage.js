2b05dfbe7c71d41a147227e013ccb9def4d4faed
cuongnp3110 <cuongnp3110@gmail.com>
2024-03-11 20:55:51 +0700

    fixed axios, add resize func

--- PredictPage/PredictPage.js
+++ PredictPage/PredictPage.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, StyleSheet, Modal } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import axios from 'axios';
+import { ImageManipulator } from 'expo-image-manipulator';
 
 // const API_ENDPOINT = 'http://10.0.2.2:3000/api/predict'; //localhost
 const API_ENDPOINT = 'https://skin-disease-detect-c718686d99c6.herokuapp.com/api/predict'; //hosting server
 
 const PictureSelectPage = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImageUri, setSelectedImageUri] = useState();
+  const [selectedImageName, setSelectedImageName] = useState();
+  const [selectedImageType, setSelectedImageType] = useState();
+  const [predict, setPredict] = useState();
+  const [prob, setProb] = useState();
 
   const selectImageHandler = async () => {
     const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -19,26 +25,31 @@ const PictureSelectPage = () => {
     if (pickerResult.cancelled === true) {
       return console.log("Cancelled");
     }
+    const resizedImage = await ImageManipulator.manipulateAsync(
+      pickerResult.assets[0].uri,
+      [{ resize: { width: 300, height: 500 } }],
+      { compress: 0.7, format: 'jpg' } // optional compression and format settings
+    );
 
-    setSelectedImage(pickerResult.assets[0].uri);
-    // console.log(pickerResult.assets[0].uri);
+    setSelectedImageUri(resizedImage.uri);
+    setSelectedImageName(pickerResult.assets[0].fileName);
+    setSelectedImageType("image/jpg");
+    console.log(selectedImageUri, selectedImageName, selectedImageType);
   };
 
   const callApiHandler = async () => {
-    if (!selectedImage) {
+    console.log(selectedImageUri, selectedImageName, selectedImageType);
+    if (!selectedImageUri || !selectedImageType || !selectedImageName) {
       alert('Please select an image first!');
       return;
     }
     try {
-      const fileData = await fetch(selectedImage.uri);
-      if (!fileData.ok) {
-        throw new Error('Failed to fetch image');
-      }
-      const fileBlob = await fileData.blob();
-      
-      // Call your API here with the selected image
       const formRequest = new FormData();
-      formRequest.append('inputImage', fileBlob);
+      formRequest.append('inputImage', {
+        uri: selectedImageUri,
+        type: selectedImageType,
+        name: selectedImageName
+      });
 
       const response = await axios({
         method: "post",
@@ -46,7 +57,7 @@ const PictureSelectPage = () => {
         data: formRequest,
         headers: { "Content-Type": "multipart/form-data" },
       })
-      console.log('API response:', response.data);
+      console.log('API response:', response.data.class);
     } catch (error) {
       console.error('Error calling API:', error);
     }
@@ -57,12 +68,30 @@ const PictureSelectPage = () => {
       <View style={styles.inputSection}>
         <Button title="Select Picture" onPress={selectImageHandler} />
       </View>
-      {selectedImage && (
+      
+      {selectedImageUri && (
         <View style={styles.imageContainer}>
-          <Image source={{ uri: selectedImage }} style={styles.image} />
+          <Image source={{ uri: selectedImageUri }} style={styles.image} />
           <Button title="Predict" onPress={callApiHandler} />
         </View>
       )}
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+            <Text>Prediction</Text>
+            <Button title="Close Modal" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+      
     </View>
   );
 };
